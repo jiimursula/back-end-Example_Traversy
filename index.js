5d441b2912c528947aa5182792cbb97f02b7158f
jiimursula <jiimursula@users.noreply.github.com>
2020-12-03 16:39:13 +0200

    Final commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const members = require('./Members');
+
+const app = express();
+
+/** 
+app.get('/', (req, res) => {
+   // res.send('<h1>Hello World!</h1>');
+   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+**/
+
+// Init middleware
+// app.use(logger);
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Homepage route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
+}));
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Set Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API routes
+app.use('/api/members', require('./routes/api/members'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
